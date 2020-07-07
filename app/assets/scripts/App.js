0eafef7d8582f78b6d9cf1400ecec83a160e8f12
elricad1308 <jc.aguilar1308@gmail.com>
2020-07-06 22:04:13 -0500

    Completed modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,11 +3,13 @@ import MobileMenu from './modules/MobileMenu';
 import ReviewOnScroll from './modules/RevealOnScroll';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 let stickyHeader = new StickyHeader();
 let mobileMenu = new MobileMenu();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
+new Modal();
 
 if(module.hot) {
     module.hot.accept()
